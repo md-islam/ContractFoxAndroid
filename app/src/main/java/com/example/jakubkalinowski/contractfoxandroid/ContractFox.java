e4699abe3e73519698a63b1de8e840c46d95cf8a
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-04-20 11:41:03 -0700

    Connection to Firebase, Created LoginActivity
    added login screen - needs testing
    redirected login screen to main activity upon the login

---
+++ app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/ContractFox.java
@@ -0,0 +1,18 @@
+package com.example.jakubkalinowski.contractfoxandroid;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by jakubkalinowski on 4/20/16.
+ */
+
+// Life-cycle of the entire app
+public class ContractFox extends android.app.Application {
+
+    // Pass Android context to Firebase
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+    }
+}
