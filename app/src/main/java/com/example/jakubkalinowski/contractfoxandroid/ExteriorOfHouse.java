daf6a6818d827aeb6ed21366d65e2eb6d11061e9
Ladimer1991 <ladimer.elyeh@gmail.com>
2016-04-22 22:24:18 -0700

    small changes to exterior of the house

--- app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/ExteriorOfHouse.java
+++ app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/ExteriorOfHouse.java
@@ -3,6 +3,7 @@ package com.example.jakubkalinowski.contractfoxandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,6 +21,7 @@ public class ExteriorOfHouse extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(ExteriorOfHouse.this, InsideGarage.class);
                 startActivity(i);
+                Log.d("garage", "button pushed");
             }
         });
     }
