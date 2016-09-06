bd95db140ddf15a7cbe13b824004850edc1954eb
Mohammed Islam <aeinul.islam@gmail.com>
2016-09-06 01:48:36 -0700

    Added new Firebase library, got login working

--- app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/registerActivity.java
+++ app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/registerActivity.java
@@ -2,6 +2,7 @@ package com.example.jakubkalinowski.contractfoxandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -14,16 +15,39 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
+//firebase deprecated library
+//import com.firebase.client.Firebase;
+//import com.firebase.client.FirebaseError;
+//firebase deprecated library
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Map;
 import java.util.regex.Pattern;
 
 public class registerActivity extends AppCompatActivity {
 
+    private static final String TAG = "authListener_TAG!!" ;
     //Firebase Reference
-    Firebase ref = new Firebase("https://contractfox.firebaseio.com/");
+  //  Firebase ref = new Firebase("https://contractfox.firebaseio.com/");
+    FirebaseDatabase mFirebaseDatabaseReference = FirebaseDatabase.getInstance();
+
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
+
+
 
 
     //variables for all the components of the activity
@@ -53,6 +77,22 @@ public class registerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        mAuth = FirebaseAuth.getInstance();
         //this part is for hint animation
         TextInputLayout firstNameWrapper = (TextInputLayout) findViewById(R.id.first_name_textInput);
         TextInputLayout lastNameWrapper = (TextInputLayout) findViewById(R.id.last_name_textInput);
@@ -72,7 +112,9 @@ public class registerActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Firebase.setAndroidContext(this);
+        //get authStateListenerInfo
+        mAuth.addAuthStateListener(mAuthListener);
+
 
         //initializing activity components
         mFirstName = (EditText) findViewById(R.id.first_name);
@@ -104,62 +146,50 @@ public class registerActivity extends AppCompatActivity {
                  * -
                  */
 
-                Toast.makeText(getApplicationContext(), "SIGN UP Button is clicked", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "SIGN UP Button is clicked", Toast.LENGTH_SHORT).show();
 
                 Boolean isExistingUsername = false;
                 firstNameInput = mFirstName.getText().toString();
                 lastNameInput = mLastName.getText().toString();
-                emailInput = mEmailAddress.getText().toString();
-                passwordInput = mPassword.getText().toString();
                 repeatPasswordInput = mRepeatPassword.getText().toString();
                 addressInput = mAddress.getText().toString();
                 contractor = mContractor.isChecked();
 
 
-                final User newUser = new User(firstNameInput,lastNameInput,emailInput, passwordInput, addressInput, contractor);
 
-                ref.createUser(newUser.getEmailAddress(), newUser.getPassword(), new Firebase.ValueResultHandler<Map<String, Object>>() {
-                    @Override
-                    public void onSuccess(Map<String, Object> result) {
-                        Toast.makeText(getApplicationContext(), "account created", Toast.LENGTH_LONG).show();
+                emailInput = mEmailAddress.getText().toString();
+                passwordInput = mPassword.getText().toString();
+                register(emailInput, passwordInput);
+             //   final User newUser = new User(firstNameInput,lastNameInput,emailInput, passwordInput, addressInput, contractor);
 
-                        /**
-                         * backup method if boolean throws issues
-                         Map <String, String> newUserMap = new HashMap<String, String>();
-                         newUserMap.put("firstName", newUser.getFirstName());
-                         newUserMap.put("lastName", newUser.getLastName());
-                         newUserMap.put("emailAddress", newUser.getEmailAddress());
-                         newUserMap.put(,"password", newUser.getPassword());
-                         newUserMap.put("address", newUser.getAddress());
-                         newUserMap.put("contractor", newUser.getContractor().toString());
-                         ref.child("users").child((String) result.get("uid")).setValue(newUserMap);
-                         *
-                         */
-
-
-
-                        ref.child("users").child((String) result.get("uid")).setValue(newUser);
-
-                        Snackbar snackbar = Snackbar.make((LinearLayout) findViewById(R.id.sign_up_layout_id), "Profile Created", Snackbar.LENGTH_LONG);
-                        snackbar.show();
-
-                    }
-
-                    @Override
-                    public void onError(FirebaseError firebaseError) {
-
-                        Log.e("ERROR TAG", "didnt work but got through firebase reference!!!: " + emailInput);
-                    }
-                });
-
-                // register button transfers to main screen
-                Intent i = new Intent(registerActivity.this, DrawerActivity.class);
-                //i.putExtra("firebaseURL", "https://allmythings2016.firebaseio.com/");
-//                i.putExtra("userEmail", email);
-                startActivity(i);
-
-
-            }
+//                ref.createUser(newUser.getEmailAddress(), newUser.getPassword(), new Firebase.ValueResultHandler<Map<String, Object>>() {
+//                    @Override
+//                    public void onSuccess(Map<String, Object> result) {
+//                        Toast.makeText(getApplicationContext(), "account created", Toast.LENGTH_LONG).show();
+//                        /**
+//                         * backup method if boolean throws issues
+//                         Map <String, String> newUserMap = new HashMap<String, String>();
+//                         newUserMap.put("firstName", newUser.getFirstName());
+//                         newUserMap.put("lastName", newUser.getLastName());
+//                         newUserMap.put("emailAddress", newUser.getEmailAddress());
+//                         newUserMap.put(,"password", newUser.getPassword());
+//                         newUserMap.put("address", newUser.getAddress());
+//                         newUserMap.put("contractor", newUser.getContractor().toString());
+//                         ref.child("users").child((String) result.get("uid")).setValue(newUserMap);
+//                         *
+//                         */
+//                        ref.child("users").child((String) result.get("uid")).setValue(newUser);
+//                        Snackbar snackbar = Snackbar.make((LinearLayout) findViewById(R.id.sign_up_layout_id), "Profile Created", Snackbar.LENGTH_LONG);
+//                        snackbar.show();
+//                    }
+//                    @Override
+//                    public void onError(FirebaseError firebaseError) {
+//                        Log.e("ERROR TAG", "didnt work but got through firebase reference!!!: " + emailInput);
+//                    }
+//                });
+             //   Intent i = new Intent(registerActivity.this, DrawerActivity.class);
+              //  startActivity(i);
+        }
         });
 
 
@@ -192,4 +222,39 @@ public class registerActivity extends AppCompatActivity {
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         return pattern.matcher(email).matches();
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+
+    //user gets registered using user's email and password, then data saving gets handled
+    //callback declared in the onCreateMethod
+    public void register(String email_address, String password){
+
+        mAuth.createUserWithEmailAndPassword(email_address, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(registerActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+
 }
