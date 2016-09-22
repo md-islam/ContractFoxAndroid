61e608cb462f4a428973f7b4da3591a0e2976fea
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-09-22 16:30:41 -0700

    Adding an estimate fragment

--- app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/ContractorProfileActivity.java
+++ app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/ContractorProfileActivity.java
@@ -2,9 +2,11 @@ package com.example.jakubkalinowski.contractfoxandroid;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.jakubkalinowski.contractfoxandroid.Contractor_Fragments.Directions;
@@ -15,9 +17,8 @@ import com.example.jakubkalinowski.contractfoxandroid.Contractor_Fragments.Websi
 public class ContractorProfileActivity extends AppCompatActivity implements Directions.OnFragmentInteractionListener,
         Reviews.OnFragmentInteractionListener, Skills.OnFragmentInteractionListener, Website.OnFragmentInteractionListener {
 
-    public Button estimateButton, callButton, directionsButton, websiteButton, skillsButton, reviewsButton;
+    private Button estimateButton, messageButton, callButton, directionsButton, websiteButton, skillsButton, reviewsButton;
 
-    public static FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +30,47 @@ public class ContractorProfileActivity extends AppCompatActivity implements Dire
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
-        // END OF GET THE TITLE PASSED TO THE TOOLBAR
 
-//        fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//        fragmentTransaction.add(pictureFragment , "pictureTag");
-//        fragmentTransaction.commit();
+        // INITIALIZING ACTIVITY COMPONENTS
+        estimateButton = (Button) findViewById(R.id.estimate_button);
+        messageButton = (Button) findViewById(R.id.message_button);
+        callButton = (Button) findViewById(R.id.call_button);
+        directionsButton = (Button) findViewById(R.id.directions_button);
+        websiteButton = (Button) findViewById(R.id.website_button);
+        //skillsButton = (Button) findViewById(R.id.skills_button);
+        //reviewsButton = (Button) findViewById(R.id.reviews_button);
+
+
+        // BUTTON ACTIONS
+        estimateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                fragmentManager = getFragmentManager();
+//                fragmentTransaction = fragmentManager.beginTransaction();
+//                fragment = new Fragment();
+//
+//                if (fragment != null) {
+//                    ft = getSupportFragmentManager().beginTransaction();
+//                    ft.replace(R.id.displayArea_ID, fragment, title);
+//                    ft.commit();
+//                }
+                Fragment estimate_fragment = new Fragment();
+
+                // Begin the transaction
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+                // Replace the contents of the container with the new fragment
+                ft.addToBackStack("Estimate");
+                ft.replace(R.id.estimate_fragment_layout, estimate_fragment,
+                        "Estimate");
+                // or ft.replace(R.id.your_placeholder, new FooFragment());
+                // Complete the changes added above
+
+                ft.commit();
+
+            }
+        });
     }
-
-    public void buttonPressed(Button button) {
-
-        int id = button.getId();
-
-    }
-
-
 
     @Override
     public void onFragmentInteraction(Uri uri) {
