650c4d39391328739e6ff1045210e5419a57001c
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-09-21 20:39:37 -0700

    Working project with DB connection
    fixed final bugs

--- app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/registerActivity.java
+++ app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/registerActivity.java
@@ -1,24 +1,40 @@
 package com.example.jakubkalinowski.contractfoxandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-
 //firebase deprecated library
 //import com.firebase.client.Firebase;
 //import com.firebase.client.FirebaseError;
 //firebase deprecated library
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class registerActivity extends AppCompatActivity {
 
@@ -125,10 +141,8 @@ public class registerActivity extends AppCompatActivity {
 
                     Fragment homeownerRegisterFragment = new RegisterHomeownerFragment();
                     homeownerRegisterFragment.setArguments(bundle);
-                    
                     // Begin the transaction
                     FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-
                     // Replace the contents of the container with the new fragment
                     ft.addToBackStack("HomeownerRegisterProfileFragment");
                     ft.replace(R.id.register_activity_framelayout, homeownerRegisterFragment,
@@ -140,6 +154,8 @@ public class registerActivity extends AppCompatActivity {
 
                 }
 
+
+
                 //This here works
 //                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 //                ft.add(R.id.login_activity_frameLayout, new SignUpFragment(), "first_fragment");
@@ -147,7 +163,7 @@ public class registerActivity extends AppCompatActivity {
             }
         });
 
-}
+    }
 
     @Override
     protected void onStart() {
@@ -200,6 +216,14 @@ public class registerActivity extends AppCompatActivity {
         }
     }
 
+
+
+
+
+
+
+
+
     /**
      * This register method is used for loggin in
      * @param email_address
@@ -226,5 +250,7 @@ public class registerActivity extends AppCompatActivity {
 //                    }
 //                });
 //    }
+
+
 
 }
