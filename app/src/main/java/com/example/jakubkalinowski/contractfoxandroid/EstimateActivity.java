27f87562997e0aaa4f40c098f43db54354facabc
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-10-12 21:27:07 -0700

    Fixed the branch. EstimateActivity in progress.

--- app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/EstimateActivity.java
+++ app/src/main/java/com/example/jakubkalinowski/contractfoxandroid/EstimateActivity.java
@@ -1,13 +1,18 @@
 package com.example.jakubkalinowski.contractfoxandroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class EstimateActivity extends AppCompatActivity {
+
+    LinearLayout layout_interior, layout_exterior, layout_backyard, layout_description;
+    RadioButton radio_interior, radio_exterior, radio_backyard;
+    TextView project_description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +21,57 @@ public class EstimateActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        layout_interior = (LinearLayout)findViewById(R.id.interior_fragment_content_layout);
+        layout_exterior = (LinearLayout)findViewById(R.id.exterior_fragment_content_layout);
+        layout_backyard = (LinearLayout)findViewById(R.id.backyard_fragment_content_layout);
+
+        layout_description = (LinearLayout)findViewById(R.id.description_text_edit_layout);
+
+        radio_interior = (RadioButton)findViewById(R.id.radio_interior);
+        radio_exterior = (RadioButton)findViewById(R.id.radio_exterior);
+        radio_backyard = (RadioButton) findViewById(R.id.radio_backyard);
+
+        project_description = (TextView)findViewById(R.id.project_description_edit_text);
+    }
+
+
+    public void onRadioButtonClicked(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.radio_interior:
+                if (checked) {
+                    radio_exterior.setChecked(false);
+                    radio_backyard.setChecked(false);
+                    layout_interior.setVisibility(View.VISIBLE);
+                    layout_exterior.setVisibility(View.GONE);
+                    layout_backyard.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.radio_exterior:
+                if (checked) {
+                    radio_interior.setChecked(false);
+                    radio_backyard.setChecked(false);
+                    layout_interior.setVisibility(View.GONE);
+                    layout_exterior.setVisibility(View.VISIBLE);
+                    layout_backyard.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.radio_backyard:
+                if (checked) {
+                    radio_interior.setChecked(false);
+                    radio_exterior.setChecked(false);
+                    layout_interior.setVisibility(View.GONE);
+                    layout_exterior.setVisibility(View.GONE);
+                    layout_backyard.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.project_description_edit_text:
+                if (checked) {
+                    layout_description.setVisibility(View.VISIBLE);
+                }
+        }
+
+
     }
 }
